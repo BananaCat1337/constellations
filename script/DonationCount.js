29d97eba45e68d08628f1f2a61e75c629354e56d
Можно просто БигДаун <148180769+BananaCat1337@users.noreply.github.com>
2024-11-15 22:22:08 +0700

    add script

---
+++ script/DonationCount.js
@@ -0,0 +1,9 @@
+async function fetchDonationCount() {
+    try {
+        const response = await fetch('http://localhost:3000/donations');// изменить на сервер, с инфой количества. 
+        const data = await response.json();
+        document.getElementById('donation-count').innerText = data.count;
+    } catch (error) {
+        console.error('Ошибка при получении количества пожертвований:', error);
+    }
+}
