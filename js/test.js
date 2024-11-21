16a16abf59731a6010785bfce5de75dfe0e0243a
BananaCat1337 <7862403@gmail.com>
2024-11-21 13:31:31 +0700

    asd

---
+++ js/test.js
@@ -0,0 +1,11 @@
+const swiper = document.querySelector.//
+[{text:'blsbla', text2:'dadad', img:'', id:1}, {text:'blsbla', text2:'dadad', img:'', id:2}].map((card)=>{
+swiper.innerHTML()
+})
+
+/**
+ * const node = document.createElement("li");
+const textnode = document.createTextNode("Water");
+node.appendChild(textnode);
+document.getElementById("myList").appendChild(node);
+ */
