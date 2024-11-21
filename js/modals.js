7cbd6f03fe42175cfc68845b747a63686027eb50
ShukaOriginal <kuraga908@mail.ru>
2024-11-21 17:03:31 +0700

    Добавил первые два попапа

--- js/modals.js
+++ js/modals.js
@@ -1,10 +1,11 @@
 const closePopUp = document.getElementsByClassName('popup_close');
 
-const openPopUp_3 = document.getElementById('popup3_open');
-const openPopUp_3_b2 = document.getElementById('popup3_open_zxc');
-const openPopUp_1 = document.getElementById('popup1_open_zxc');
+const openPopUp_1 = document.getElementById('popup1_open');
+const openPopUp_2 = document.getElementById('popup2_open');
+const openPopUp_3 = document.getElementsByClassName('popup3_open');
 
 const popUp1 = document.getElementById('popUp1')
+const popUp2 = document.getElementById('popUp2')
 const popUp3 = document.getElementById('popUp3')
 const popUp = document.getElementById('popUp')
 
@@ -19,24 +20,17 @@ for (var i = 0 ; i < closePopUp.length; i++) {
 
         countWindows = 0
     })
- }
+}
 
-
-openPopUp_3.addEventListener('click', function(e){
-    countWindows += 1
-    openPopUps()
-    e.preventDefault()
-    popUp3.classList.add('active')
-    popUp.classList.add('active')
-})
-
-openPopUp_3_b2.addEventListener('click', function(e){
-    countWindows += 1
-    openPopUps()
-    e.preventDefault()
-    popUp3.classList.add('active')
-    popUp.classList.add('active')
-})
+for (var i = 0 ; i < openPopUp_3.length; i++) {
+    openPopUp_3[i].addEventListener('click', function(e){
+        countWindows += 1
+        openPopUps()
+        e.preventDefault()
+        popUp3.classList.add('active')
+        popUp.classList.add('active')
+    })
+}
 
 openPopUp_1.addEventListener('click', function(e){
     countWindows += 1
@@ -46,9 +40,18 @@ openPopUp_1.addEventListener('click', function(e){
     popUp.classList.add('active')
 })
 
+openPopUp_2.addEventListener('click', function(e){
+    countWindows += 1
+    openPopUps()
+    e.preventDefault()
+    popUp2.classList.add('active')
+    popUp.classList.add('active')
+})
+
 function openPopUps() {
     if (countWindows > 1) {
         popUp1.classList.remove('active')
+        popUp2.classList.remove('active')
         popUp3.classList.remove('active')
     }
 }
