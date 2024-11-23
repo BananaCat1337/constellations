651ca72615b77d1b90bf026486a62d4e55b5ce3b
BananaCat1337 <7862403@gmail.com>
2024-11-23 18:07:27 +0700

    Change video.js

--- js/video.js
+++ js/video.js
@@ -1,6 +1,10 @@
+const video = document.getElementById('videoElement');
+
 document.getElementById("videoElement").addEventListener("click", function () {
     if (this.paused) {
-      this.play();  // Запускает видео
+      this.play();
+      video.setAttribute('controls', 'controls');
+        // Запускает видео
     } else {
       this.pause();  // Ставит видео на паузу
     }
