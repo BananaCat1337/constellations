c4ebde46117f01398be34d16ba5115a0c3f525d9
BananaCat1337 <7862403@gmail.com>
2024-11-23 18:09:50 +0700

    delete testButton

--- js/video.js
+++ js/video.js
@@ -4,8 +4,7 @@ document.getElementById("videoElement").addEventListener("click", function () {
     if (this.paused) {
       this.play();
       video.setAttribute('controls', 'controls');
-        // Запускает видео
     } else {
-      this.pause();  // Ставит видео на паузу
+      this.pause();
     }
   });
