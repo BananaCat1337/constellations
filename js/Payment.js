57cdc7eeec4c5986a214d6c79521d5e79ae2578e
BananaCat1337 <7862403@gmail.com>
2024-11-24 18:05:20 +0700

    fixDonate

--- js/Payment.js
+++ js/Payment.js
@@ -6,57 +6,34 @@ let payment_email = ''
 let payment_Name = ''
 
 
+
 this.pay = function () {
-    var widget = new cp.CloudPayments({
+    var payments = new cp.CloudPayments({
         language: "ru-RU",
         email: "",
-        applePaySupport: true,
-        googlePaySupport: true,
-        yandexPaySupport: true,
-        tinkoffPaySupport: true,
+        applePaySupport: false,
+        googlePaySupport: false,
+        yandexPaySupport: false,
+        tinkoffPaySupport: false,
         tinkoffInstallmentSupport: false,
         sbpSupport: true
+    })
+    
+    payments.pay("charge", {
+        publicId: "test_api_00000000000000000000002",
+        description: "Тестовая оплата",
+        amount: payment_amount,
+        currency: "RUB",
+        invoiceId: "123",
+        accountId: "123",
+        email: payment_email,
+        skin: "classic",
+        requireEmail: false,
+    }).then(function(widgetResult) {
+        console.log('result', widgetResult);
+    }).catch(function(error) {
+        console.log('error', error);
     });
-       widget.pay('charge', // или 'charge'
-           { //options
-               publicId: 'pk_ccdef71088be717d883027ce6ba12', //id из личного кабинета
-               description: 'Зажгите созвездие', //назначение
-               amount: payment_amount, //сумма
-               currency: 'RUB', //валюта
-               accountId: payment_email, //идентификатор плательщика (необязательно)
-               invoiceId: '1234567', //номер заказа  (необязательно)
-               email: payment_email, //email плательщика (необязательно)
-               skin: "classic", //дизайн виджета (необязательно)
-               data: {
-                   myProp: 'myProp value'
-               },
-               configuration: {
-                   common: {
-                       successRedirectUrl: "https://{ваш сайт}/success", // адреса для перенаправления 
-                       failRedirectUrl: "https://{ваш сайт}/fail"        // при оплате по T-Pay
-                   }
-               },
-               payer: { 
-                   Name: payment_Name,
-                   address: 'тестовый проезд дом тест',
-               }
-           },
-           {
-               onSuccess: function (options) { // success
-                   //действие при успешной оплате
-               },
-               onFail: function (reason, options) { // fail
-                console.log(reason);
-                console.log(options);
-                
-                
-                   //действие при неуспешной оплате
-               },
-               onComplete: function (paymentResult, options) { //Вызывается как только виджет получает от api.cloudpayments ответ с результатом транзакции.
-                   //например вызов вашей аналитики
-               }
-           }
-       )
    };
 
 
