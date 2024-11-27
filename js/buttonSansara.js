db9bdea217b51daf7429c01ef6b3f48913984392
sryhanzom4in <sryhanzom4in@yandex.ru>
2024-11-27 13:11:25 +0700

    little refactoring

--- js/buttonSansara.js
+++ js/buttonSansara.js
@@ -1,14 +1,17 @@
-const rangeInput = document.getElementById("range");
-const thumb = document.querySelector(".range-text");
+document.addEventListener('DOMContentLoaded', () => {
+  const rangeInput = document.getElementById("range");
+  const thumb = document.querySelector(".range-text");
 
-const updateThumb = () => {
-  const value = rangeInput.value;
-  thumb.textContent = `|| ${value}° ||`;
-  const percentage =
-    (value - rangeInput.min) / (rangeInput.max - rangeInput.min);
-  const offset = percentage * (rangeInput.clientWidth - 80);
-  thumb.style.left = `${offset}px`;
-};
+  const updateThumb = () => {
+    const value = rangeInput.value;
+    thumb.textContent = `|| ${value}° ||`;
+    const percentage =
+      (value - rangeInput.min) / (rangeInput.max - rangeInput.min);
+    const offset = percentage * (rangeInput.clientWidth - 80);
+    thumb.style.left = `${offset}px`;
+  };
 
-rangeInput.addEventListener("input", updateThumb);
-document.onload(updateThumb());
+  rangeInput.addEventListener("input", updateThumb);
+  // document.onload(updateThumb()); //???????
+
+})
--- script/DonationCount.js
+++
@@ -1,9 +0,0 @@
-async function fetchDonationCount() {
-    try {
-        const response = await fetch('http://localhost:3000/donations');// изменить на сервер, с инфой количества. 
-        const data = await response.json();
-        document.getElementById('donation-count').innerText = data.count;
-    } catch (error) {
-        console.error('Ошибка при получении количества пожертвований:', error);
-    }
-}
