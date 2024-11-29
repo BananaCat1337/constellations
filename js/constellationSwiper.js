e21ae2d1512f85130aa9c5394f7d40529f402cbf
sryhanzom4in <sryhanzom4in@yandex.ru>
2024-11-29 20:25:58 +0700

    scroll changes coming

--- js/constellationSwiper.js
+++ js/constellationSwiper.js
@@ -1,36 +1,22 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
-console.log(Swiper)
-
-document.addEventListener('DOMContentLoaded', () => {
-  const swiper = new Swiper('.constellation__slider', {
-    observer: true,
-    observeParents: true,
+document.addEventListener("DOMContentLoaded", () => {
+  let swiper = new Swiper(".constellation__slider", {
     slidesPerView: 1,
-    slideToClickedSlide: false,
-    spaceBetween: 0,
+    allowTouchMove: false,
+    spaceBetween: 90,
     watchOverflow: true,
-    speed: 800,
+    speed: 400,
     loop: false,
     pagination: false,
     navigation: {
-      nextEl: '.constellation__slider_button-next',
-      prevEl: '.constellation__slider_button-prev',
+      nextEl: ".constellation__slider_button-next",
+      prevEl: ".constellation__slider_button-prev",
     },
   });
-  const hiddenSlides = document.querySelectorAll('.hideConstellationGroup__slider')
-  const checkCurrentSlideID = () => {
-    for (const slide of document.querySelectorAll(constellationGroup__slider)) {
-      let i = 0;
-      if (swiper.clickedIndex !== i) {
-        i = swiper.clickedIndex;
-      }
-    }
-  }
+
+  const hiddenSlides = document.querySelectorAll(".hideConstellationGroup__slider");
   setTimeout(() => {
     hiddenSlides.forEach((el) => {
-      el.classList.remove('hideConstellationGroup__slider')
-    })
-  },
-    200)
-})
-
+      el.classList.remove("hideConstellationGroup__slider");
+    });
+  }, 200);
+});
