a24f860ebbb5f299fa4afd36da9ab0619339b428
BananaCat1337 <7862403@gmail.com>
2024-11-29 14:14:33 +0700

    Скрипт Поделиться

--- js/constellationsName.js
+++ js/constellationsName.js
@@ -1,14 +1,16 @@
-  const constellationTitle = document.getElementById('constellation-title');
-  const constellationTitleShare = document.getElementById('constellation-title-share');
-  const constellationCards = document.querySelectorAll('.cardContainer');
-  const popUp = document.querySelector('#popUp-sozvezdie');
-  const constellationImage = popUp.querySelector('.constellationImage');
-  constellationCards.forEach(card => {
-    card.addEventListener('click', () => {
-      const newTitle = card.getAttribute('data-constellation');
-      const constellation = card.getAttribute('img-constellation');
-      constellationImage.src = `/cards/childInfoCards/childInfoSvgs/${constellation}.svg`;
-      constellationTitle.textContent = `${newTitle}`;
-      constellationTitleShare.textContent = `${newTitle}`;
-    });
+const constellationTitle = document.getElementById("constellation-title");
+const constellationTitleShare = document.getElementById(
+  "constellation-title-share"
+);
+const constellationCards = document.querySelectorAll(".cardContainer");
+const popUp = document.querySelector("#popUp-sozvezdie");
+const constellationImage = popUp.querySelector(".constellationImage");
+constellationCards.forEach((card) => {
+  card.addEventListener("click", () => {
+    const newTitle = card.getAttribute("data-constellation");
+    const constellation = card.getAttribute("img-constellation");
+    constellationImage.src = `./cards/childInfoCards/childInfoSvgs/${constellation}.svg`;
+    constellationTitle.textContent = `${newTitle}`;
+    constellationTitleShare.textContent = `${newTitle}`;
   });
+});
