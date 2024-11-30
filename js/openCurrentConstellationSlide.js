8558bde14207fc443bb0f9dc5f90b8b4468610b8
BananaCat1337 <7862403@gmail.com>
2024-11-30 21:00:13 +0700

    fix CardSwiper

--- js/openCurrentConstellationSlide.js
+++ js/openCurrentConstellationSlide.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const cardContainers = document.querySelectorAll(".cardContainer");
   const popUp3 = document.getElementById("popUp3");
   const popUp = document.getElementById("popUp");
+  const backsideCard = document.querySelectorAll(".backsideBody"); 
 
   const swiperCards = new Swiper(".card__content", {
     loop: true,
@@ -22,6 +23,8 @@ document.addEventListener("DOMContentLoaded", () => {
     initialSlide: 2,
     slidesPerView: 3,
     spaceBetween: 50,
+    observer: true,
+    observeParents: true,
     breakpoints: {
       200: { slidesPerView: 1 },
       500: { slidesPerView: 3 },
@@ -32,10 +35,15 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   });
 
+  swiperCards.on('slideChange', () => {
+    document.querySelectorAll('.backsideBody').forEach((el) => {
+      el.style.transform = 'rotateY(180deg)';
+    });
+  });
+
   constellations.forEach((el) => {
     el.addEventListener("click", () => {
       const targetConstellation = el.getAttribute("data-constellation");
-
       const targetCardIndex = [...cardContainers].findIndex((card) => card.getAttribute("data-constellation") === targetConstellation);
 
       if (targetCardIndex !== -1) {
