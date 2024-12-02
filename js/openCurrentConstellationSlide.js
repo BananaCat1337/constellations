eeaa2d94dbbc499d79b4fd69430eda31becd1c3e
sryhanzom4in <sryhanzom4in@yandex.ru>
2024-12-02 12:51:44 +0700

    fixCurrentConstellationClick

--- js/openCurrentConstellationSlide.js
+++ js/openCurrentConstellationSlide.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const cardContainers = document.querySelectorAll(".cardContainer");
   const popUp3 = document.getElementById("popUp3");
   const popUp = document.getElementById("popUp");
-  const backsideCard = document.querySelectorAll(".backsideBody"); 
+  const backsideCard = document.querySelectorAll(".backsideBody");
 
   const swiperCards = new Swiper(".card__content", {
     loop: true,
@@ -23,8 +23,6 @@ document.addEventListener("DOMContentLoaded", () => {
     initialSlide: 2,
     slidesPerView: 3,
     spaceBetween: 50,
-    observer: true,
-    observeParents: true,
     breakpoints: {
       200: { slidesPerView: 1 },
       500: { slidesPerView: 3 },
