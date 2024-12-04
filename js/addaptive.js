b4463b98d519a5e860b85374ca466e9aa5a75325
ShukaOriginal <kuraga908@mail.ru>
2024-12-04 22:10:55 +0700

    lower popUp mobile fix 5

--- js/addaptive.js
+++ js/addaptive.js
@@ -1,5 +1,6 @@
 
 let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 window.addEventListener('resize', () => {
     // We execute the same script as before
