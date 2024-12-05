639b982abb1bc1484d8f32543e5ed5ecb764f2c2
BananaCat1337 <7862403@gmail.com>
2024-12-06 00:58:20 +0700

    backpay

--- js/Payment.js
+++ js/Payment.js
@@ -43,8 +43,7 @@ const pay = function () {
       requireEmail: false,
     })
     .then((widgetResult) => {
-      // if (widgetResult.status === "success") 
-        sozvezdie(payment_Name);
+      if (widgetResult.status === "success") sozvezdie(payment_Name);
     })
     .catch(function (error) {
       console.log("error", error);
