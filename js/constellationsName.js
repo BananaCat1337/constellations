5a4955116375ae9066a6ab4bcb76fa00ebe3f1be
BananaCat1337 <7862403@gmail.com>
2024-12-07 14:12:01 +0700

    shange share

--- js/constellationsName.js
+++ js/constellationsName.js
@@ -9,7 +9,7 @@ const resetButton = document.querySelector(".defShare");
 const resetButtonMobyle = document.querySelector(".defShare-mobyle");
 const shareText = `Благотворительный фонд "Настенька" попросил подопечных нарисовать свои заветные мечты. Эти рисунки обратились в настоящие созвездия, которые в этот новый год Мы можем сделать ярче! Все просто - следуй за созвездием мечты.`;
 const url = "https://mechta.nastenka.ru/";
-const defaultImage = "https://mechta.nastenka.ru/assets/share/defShare.jpg";
+const defaultImage = "https://mechta.nastenka.ru/assets/share/Share.jpg";
 document.getElementById(
   "vk-share"
 ).href = `https://vk.com/share.php?url=${encodeURIComponent(url)}&title=Следуй за созвездием мечты&og:description=${encodeURIComponent(shareText)}&image=${defaultImage}`;
@@ -20,34 +20,6 @@ document.getElementById(
   "tg-share"
 ).href = `https://t.me/share/url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(shareText)}`
 
-resetButton.addEventListener("click", () => {
-  document.getElementById(
-    "ok-share"
-  ).href = `https://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl=${encodeURIComponent(url)}&st.comments=${encodeURIComponent(shareText)}&st.imageUrl=${defaultImage}`;
-
-  document.getElementById(
-    "vk-share"
-  ).href = `https://vk.com/share.php?url=${encodeURIComponent(url)}&title=Следуй за созвездием мечты&description=${encodeURIComponent(shareText)}&image=${defaultImage}`;
-
-  document.getElementById(
-    "tg-share"
-  ).href = `https://t.me/share/url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(shareText)}`;
-});
-
-resetButtonMobyle.addEventListener("click", () => {
-  document.getElementById(
-    "ok-share"
-  ).href = `https://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl=${encodeURIComponent(url)}&st.comments=${encodeURIComponent(shareText)}&st.imageUrl=${defaultImage}`;
-
-  document.getElementById(
-    "vk-share"
-  ).href = `https://vk.com/share.php?url=${encodeURIComponent(url)}&title=Следуй за созвездием мечты&description=${encodeURIComponent(shareText)}&image=${defaultImage}`;
-
-  document.getElementById(
-    "tg-share"
-  ).href = `https://t.me/share/url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(shareText)}`;
-});
-
 constellationCards.forEach((card) => {
   card.addEventListener("click", () => {
     const newTitle = card.getAttribute("data-constellation");
@@ -55,18 +27,5 @@ constellationCards.forEach((card) => {
     constellationImage.src = `./cards/childInfoCards/childInfoSvgs/${constellation}.svg`;
     constellationTitle.textContent = `${newTitle}`;
     constellationTitleShare.textContent = `${newTitle}`;
-    const imagePath = `https://mechta.nastenka.ru/assets/share/${constellation}.jpg`;
-
-    document.getElementById(
-      "ok-share"
-    ).href = `https://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl=${encodeURIComponent(url)}&st.comments=${encodeURIComponent(shareText)}&st.imageUrl=${imagePath}`;
-
-    document.getElementById(
-      "vk-share"
-    ).href = `https://vk.com/share.php?url=${encodeURIComponent(url)}&title=Следуй за созвездием мечты&description=${encodeURIComponent(shareText)}&image=${imagePath}`;
-
-    document.getElementById(
-      "tg-share"
-    ).href = `https://t.me/share/url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(shareText)}`;
   });
 });
