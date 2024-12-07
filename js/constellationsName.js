c87e3f88492ec4b9e37207a53750ec8d5014b7db
BananaCat1337 <7862403@gmail.com>
2024-12-07 16:34:33 +0700

    Rustam LEGENDA

--- js/constellationsName.js
+++ js/constellationsName.js
@@ -5,20 +5,24 @@ const constellationTitleShare = document.getElementById(
 const constellationCards = document.querySelectorAll(".cardContainer");
 const popUp = document.querySelector("#popUp-sozvezdie");
 const constellationImage = popUp.querySelector(".constellationImage");
-const resetButton = document.querySelector(".defShare");
-const resetButtonMobyle = document.querySelector(".defShare-mobyle");
 const shareText = `Благотворительный фонд "Настенька" попросил подопечных нарисовать свои заветные мечты. Эти рисунки обратились в настоящие созвездия, которые в этот новый год Мы можем сделать ярче! Все просто - следуй за созвездием мечты.`;
 const url = "https://mechta.nastenka.ru/";
 const defaultImage = "https://mechta.nastenka.ru/assets/share/Share.jpg";
 document.getElementById(
   "vk-share"
-).href = `https://vk.com/share.php?url=${encodeURIComponent(url)}&title=Следуй за созвездием мечты&og:description=${encodeURIComponent(shareText)}&image=${defaultImage}`;
+).href = `https://vk.com/share.php?url=${encodeURIComponent(
+  url
+)}&title=Следуй за созвездием мечты&image=${encodeURIComponent(defaultImage)}`;
 document.getElementById(
   "ok-share"
-).href = `https://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl=${encodeURIComponent(url)}&st.comments=${encodeURIComponent(shareText)}&st.imageUrl=${defaultImage}`;
+).href = `https://connect.ok.ru/dk?st.cmd=WidgetSharePreview&st.shareUrl=${encodeURIComponent(
+  url
+)}&st.comments=${encodeURIComponent(shareText)}&st.imageUrl=${defaultImage}`;
 document.getElementById(
   "tg-share"
-).href = `https://t.me/share/url?url=${encodeURIComponent(url)}&text=${encodeURIComponent(shareText)}`
+).href = `https://t.me/share/url?url=${encodeURIComponent(
+  url
+)}&text=${encodeURIComponent(shareText)}`;
 
 constellationCards.forEach((card) => {
   card.addEventListener("click", () => {
