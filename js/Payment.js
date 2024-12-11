fc0fc4ae97c4fd5769a61d474906aadf27528137
BananaCat1337 <7862403@gmail.com>
2024-12-11 11:17:57 +0700

    test2

--- js/Payment.js
+++ js/Payment.js
@@ -33,7 +33,7 @@ const pay = function () {
     yandexPaySupport: false,
     tinkoffPaySupport: false,
     tinkoffInstallmentSupport: false,
-    sbpSupport: false,
+    sbpSupport: true,
   });
 
   payments
