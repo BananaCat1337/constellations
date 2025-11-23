c75abb588b83ecd0eea72742363871cdf18071aa
BananaCat1337 <7862403@gmail.com>
2025-11-23 14:08:58 +0700

    check this pro codding in my style

--- js/imageZoom.js
+++ js/imageZoom.js
@@ -2,9 +2,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll(".foto_for_pc, .foto_for_mobile");
 
     images.forEach(img => {
+
+        img.style.cursor = "pointer";
+
         img.style.transition = "transform 0.4s ease";
 
-        img.addEventListener("click", () => {
+        img.addEventListener("click", (e) => {
+            e.stopPropagation();
+        
             const isZoomed = img.classList.contains("zoomed");
 
             if (!isZoomed) {
@@ -15,5 +20,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
             img.classList.toggle("zoomed");
         });
+    });
+
+    document.addEventListener("click", () => {
+        const zoomed = document.querySelector(".zoomed");
+        if (!zoomed) return;
+
+        zoomed.style.transform = "scale(1)";
+        zoomed.classList.remove("zoomed");
     });
 });
